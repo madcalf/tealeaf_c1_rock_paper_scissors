474c8d6a05b2e2314b456b64ac5d28f1f4d3a174
madcalf <ddd@DsMacBookPro-4.local>
2014-08-26 18:35:50 -0400

    Incorporate some ideas from solution vid
    - change outer game loop from while loop to loop do
    - change input validation from while to begin/end/while to avoid some repitition
    - make options hash var name uppercase since it's a constant
    - use array#include to validate input instead of the regexp match
    - downcase input immediately instead of waiting til validation test
    - use array#sample instead of Random#rand for computer option selection

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,60 +1,51 @@
 # Course 1. Lesson 1. Rock, Paper, Scissors
 
-options = {}
-options["r"] = { "name" => "rock",  "target" => "scissors", "action" => "crushes"}
-options["p"] = { "name" => "paper",  "target" => "rock", "action" => "wraps"}
-options["s"] = { "name" => "scissors",  "target" => "paper", "action" => "cut"}
+OPTIONS = {}
+OPTIONS["r"] = { "name" => "rock",  "target" => "scissors", "action" => "crushes"}
+OPTIONS["p"] = { "name" => "paper",  "target" => "rock", "action" => "wraps"}
+OPTIONS["s"] = { "name" => "scissors",  "target" => "paper", "action" => "cut"}
 
-# not sure why this isn't working to clear the screen
-system 'clear'
+score = {player:0, computer:0}
 
-puts
-puts "---------------------------"
-puts "Play Rock Paper Scissors!"
-puts "---------------------------"
-
-wins = {player:0, computer:0}
-
-playing = true
-while playing
-  puts 
-  puts "Choose your weapon: [R]ock  [P]aper  [S]cissors"
-  input = gets.chomp
+loop do
+  # not sure why this isn't working to clear the screen
+  system 'clear'   
+  puts "\n---------------------------"
+  puts "Play Rock Paper Scissors!"
+  puts "---------------------------" 
   # make sure user selects R, P or S only
-  while (input.size !=1) || !input.downcase.match(/[rps]/) 
-    puts "Not a valid selection. Select R, P, or S" 
-    input = gets.chomp    
-  end
+  begin
+    puts "Choose your weapon: [R]ock  [P]aper  [S]cissors"
+    input = gets.chomp.downcase
+    break if OPTIONS.keys.include?(input)
+    puts "'#{input}' is not an option."
+  end while true
   
-  player_choice = options[input]         
+  player_choice = OPTIONS[input]         
   puts "You chose #{player_choice['name'].capitalize}"
 
-  # computer selects random 
-  r = Random.new.rand(0..options.size - 1)
-  computer_choice = options.values[r]
+  # computer selects random one
+  computer_choice = OPTIONS.values.sample
   puts "I chose #{computer_choice["name"].capitalize}"
 
   # test for the winner
-  if player_choice == computer_choice
+  if player_choice["name"] == computer_choice["name"]
     puts "It's a tie!"
   elsif player_choice["target"] == computer_choice["name"]
     puts "#{player_choice["name"].capitalize} #{player_choice["action"]} #{player_choice["target"].capitalize}"
     puts "You Win!"
-    wins[:player] += 1
+    score[:player] += 1
   else
     puts "#{computer_choice["name"].capitalize} #{computer_choice["action"]} #{computer_choice["target"].capitalize}"
     puts "Ha! I Win!!!"
-    wins[:computer] += 1
+    score[:computer] += 1
   end
   
-  puts "Score: You: #{wins[:player]}  Me: #{wins[:computer]}"
+  puts "Score: You: #{score[:player]}  Me: #{score[:computer]}"
   
   # prompt to play again
   puts "\nPlay again?  [Y]es  [N]o"
-  input = gets.chomp
-  if input.downcase != "y"
-    puts "Ok, thanks for playing!" 
-    playing = false 
-  end
+  break if gets.chomp.downcase != "y"  
 end 
  
+puts "Ok, thanks for playing!" 
